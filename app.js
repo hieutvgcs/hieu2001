b2bf18614231b899f9ed34e08aae623c57772f58
Hieu2001 <hieu2001@gmail.com>
2021-10-08 20:14:01 +0700

    0

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const http = require('http'); 
+const hostname = "localhost";
 
-const port = process.env.PORT || 3000 
+const port = process.env.PORT || 8000;
+var solan = 0;
 
  
 
@@ -9,6 +11,7 @@ const server = http.createServer((req, res) => {
   res.statusCode = 200; 
 
   res.setHeader('Content-Type', 'text/html'); 
+  
 
   res.end('<h1>Hello World</h1>'); 
 
